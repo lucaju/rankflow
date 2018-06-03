e9bd9c7ffc54e9c8d371d589805afc5254337de0
Luciano Frizzera <lucaju@me.com>
2018-06-02 22:55:15 -0400

    updates

--- rankflowVis.js
+++ rankflowVis.js
@@ -7,7 +7,12 @@
         this.windowWidth = 900;
         this.resizeTimer = null;
 
-        this.margin = {top:0, bottom: 0, left: 0, right: 0};
+        this.margin = {
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0
+        };
         this.width = 800;
         this.height = 500;
 
